e6855b7ee010be7589d863ac09215b0750bdd186
Danila Gallo <danila.g@hotmail.com>
2016-05-03 00:04:24 -0300

    Subo gitignore

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World!");
+            MessageBox.Show("Hello World bitch!");
         }
     }
 }
