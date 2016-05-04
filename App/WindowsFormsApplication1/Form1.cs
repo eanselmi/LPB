8f647137c95e55018ef14574e40486b2f52a1c57
eanselmi <nazareno.anselmi@gmail.com>
2016-05-03 23:23:20 -0300

    excluyo obj del proyecto

--- App/WindowsFormsApplication1/Form1.cs
+++ App/WindowsFormsApplication1/Form1.cs
@@ -20,6 +20,7 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello World bitch!");
+            button1.Enabled = false;
         }
     }
 }
