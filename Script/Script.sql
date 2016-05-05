f08310c1fa5c47231b47476ad122524f2d019257
eanselmi <nazareno.anselmi@gmail.com>
2016-05-05 13:21:40 -0300

    Cargo datos iniciales de las tablas usuarios, tipodeusuario, roles, funcionalidades, funcionalidadesxrol, rolesxusuario

--- Script/Script.sql
+++ Script/Script.sql
@@ -12,15 +12,9 @@ GO
 /*---------------Limpieza de Tablas-------------*/
 /*---------Si la tabla ya existe la borro-------------*/
 
-IF OBJECT_ID('LPB.Usuarios') IS NOT NULL
+IF OBJECT_ID('LPB.FuncionalidadesxRoles') IS NOT NULL
 BEGIN
-        DROP TABLE LPB.Usuarios ;
-END;
-GO
-
-IF OBJECT_ID('LPB.TiposDeUsuario') IS NOT NULL
-BEGIN
-        DROP TABLE LPB.TiposDeUsuario ;
+        DROP TABLE LPB.FuncionalidadesxRoles ;
 END;
 GO
 
@@ -30,9 +24,15 @@ BEGIN
 END;
 GO
 
-IF OBJECT_ID('LPB.FuncionalidadesxRoles') IS NOT NULL
+IF OBJECT_ID('LPB.Usuarios') IS NOT NULL
 BEGIN
-        DROP TABLE LPB.FuncionalidadesxRoles ;
+        DROP TABLE LPB.Usuarios ;
+END;
+GO
+
+IF OBJECT_ID('LPB.TiposDeUsuario') IS NOT NULL
+BEGIN
+        DROP TABLE LPB.TiposDeUsuario ;
 END;
 GO
 
@@ -54,7 +54,7 @@ CREATE TABLE [LPB].Usuarios(
 id INT NOT NULL IDENTITY(1,1),
 TipoUsuario_id INT NOT NULL,
 username VARCHAR(45) NOT NULL,
-pass VARCHAR(45) NOT NULL,
+pass VARCHAR(100) NOT NULL,
 habilitado BIT DEFAULT 1,
 cantIntentosFallidos INT DEFAULT 0,
 nuevo BIT DEFAULT 1,
@@ -80,7 +80,7 @@ habilitado BIT DEFAULT 1,
 PRIMARY KEY(id));
 
 CREATE TABLE [LPB].Funcionalidades(
-id INT NOT NULL,
+id INT NOT NULL IDENTITY(1,1),
 descripcion varchar(45) NOT NULL,
 PRIMARY KEY(id));
 
@@ -107,3 +107,74 @@ ALTER TABLE LPB.FuncionalidadesxRoles ADD
             FOREIGN KEY (Roles_id) references LPB.roles;
                                 
 
+/*---------Carga de datos / migracion--------------------*/
+
+
+/*Tipos de usuario*/
+BEGIN TRANSACTION
+INSERT INTO LPB.TiposDeUsuario (descripcion) VALUES ('Administrador');
+INSERT INTO LPB.TiposDeUsuario (descripcion) VALUES ('Cliente');
+INSERT INTO LPB.TiposDeUsuario (descripcion) VALUES ('Empresa');
+COMMIT;
+
+
+/*Roles*/
+BEGIN TRANSACTION
+INSERT INTO LPB.Roles (nombre) VALUES ('Administrador');
+INSERT INTO LPB.Roles (nombre) VALUES('Cliente');
+INSERT INTO LPB.Roles (nombre) VALUES('Empresa');
+COMMIT;
+
+
+/*Funcionalidades*/
+BEGIN TRANSACTION
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('abmRol');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('abmUsuario');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('abmRubro');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('abmVisibilidad');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('generarPublicacion');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('comprar/ofertar');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('historialCliente');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('calificarVendedor');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('consultaFacturas');
+INSERT INTO LPB.Funcionalidades (descripcion) VALUES ('listadoEstadistico');
+COMMIT;
+
+
+/*Funcionalidad por rol*/
+GO
+DECLARE @ID INTEGER;
+SET @ID = (SELECT id FROM [LPB].Roles WHERE nombre='Administrador');
+
+BEGIN TRANSACTION
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (1, @ID);
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (2, @ID);
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (3, @ID);
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (4, @ID);
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (10, @ID);
+
+SET @ID = (SELECT id FROM [LPB].Roles WHERE nombre='Cliente');
+
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (5, @ID);
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (6, @ID);
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (7, @ID);
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (8, @ID);
+
+SET @ID = (SELECT id FROM [LPB].Roles WHERE nombre='Empresa');
+
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (5, @ID);
+INSERT INTO LPB.FuncionalidadesxRoles (Funcionalidades_id, Roles_id) VALUES (9, @ID);
+
+COMMIT;
+
+
+/*creacion Usuarios -HASH del password w23e: 'e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0dc9be7'*/
+BEGIN TRANSACTION
+INSERT INTO LPB.Usuarios (TipoUsuario_id, username, pass, nuevo) VALUES(1,'admin','e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7',0);
+COMMIT;
+
+
+/*Roles Por Usuario*/
+BEGIN TRANSACTION
+INSERT INTO LPB.RolesxUsuarios(Roles_id, Usuarios_id) VALUES (1,1);
+COMMIT;
