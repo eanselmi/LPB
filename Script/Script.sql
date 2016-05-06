3a673dd67c9a1cf4ad322d377b310bfc45d85b10
eanselmi <nazareno.anselmi@gmail.com>
2016-05-06 16:33:36 -0300

    Agrego migracion de usuarios y rolesxusuarios desde la tabla maestra

--- Script/Script.sql
+++ Script/Script.sql
@@ -47,8 +47,8 @@ BEGIN
         DROP TABLE LPB.Funcionalidades ;
 END;
 GO
-/*---------Definiciones de Tabla-------------*/
 
+/*---------Definiciones de Tabla-------------*/
 
 CREATE TABLE [LPB].Usuarios(
 id INT NOT NULL IDENTITY(1,1),
@@ -59,41 +59,58 @@ habilitado BIT DEFAULT 1,
 cantIntentosFallidos INT DEFAULT 0,
 nuevo BIT DEFAULT 1,
 PRIMARY KEY(id));
+GO
 
 CREATE TABLE [LPB].RolesxUsuarios(
 Roles_id INT NOT NULL,
 Usuarios_id INT NOT NULL,
 PRIMARY KEY(Roles_id, Usuarios_id));
+GO
 
 CREATE TABLE [LPB].Roles(
 id INT NOT NULL IDENTITY(1,1),
 nombre VARCHAR(45) NOT NULL,
 habilitado BIT DEFAULT 1,
 PRIMARY KEY(id));
+GO
 
 CREATE TABLE [LPB].Funcionalidades(
 id INT NOT NULL IDENTITY(1,1),
 descripcion varchar(45) NOT NULL,
 PRIMARY KEY(id));
+GO
 
 CREATE TABLE [LPB].FuncionalidadesxRoles(
 Funcionalidades_id INT NOT NULL,
 Roles_id INT NOT NULL,
 PRIMARY KEY(Funcionalidades_id, Roles_id));
-
-
-
+GO
 /*---------Definiciones de FK-------*/
 
 
 ALTER TABLE LPB.RolesxUsuarios ADD
             FOREIGN KEY (Roles_id) references LPB.Roles,
             FOREIGN KEY (Usuarios_id) references LPB.Usuarios;
+GO
                                                             
 ALTER TABLE LPB.FuncionalidadesxRoles ADD
             FOREIGN KEY (Funcionalidades_id) references LPB.Funcionalidades,
             FOREIGN KEY (Roles_id) references LPB.roles;
-                                
+GO
+
+/* Declaración de variables */
+
+DECLARE @DocumentoCodigo_Dni VARCHAR(10) = 'DNI'
+DECLARE @DocumentoCodigo_Cuil VARCHAR(10) = 'CUIL'
+DECLARE @DocumentoCodigo_Cuit VARCHAR(10) = 'CUIT'
+
+DECLARE @TipoDeUsuario_Cliente VARCHAR(15) = 'Cliente'
+DECLARE @TipoDeUsuario_Empresa VARCHAR(15) = 'Empresa'
+DECLARE @TipoDeUsuario_Administrador VARCHAR(15) = 'Administrador'
+
+DECLARE @RolId_Administrador INT = 1
+DECLARE @RolId_Cliente INT = 2
+DECLARE @RolId_Empresa INT = 3
 
 /*---------Carga de datos / migracion--------------------*/
 
@@ -122,7 +139,6 @@ COMMIT;
 
 
 /*Funcionalidad por rol*/
-GO
 DECLARE @ID INTEGER;
 SET @ID = (SELECT id FROM [LPB].Roles WHERE nombre='Administrador');
 
@@ -153,8 +169,40 @@ BEGIN TRANSACTION
 INSERT INTO LPB.Usuarios (TipoUsuario, username, pass, nuevo) VALUES('Administrador','admin','e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7',0);
 COMMIT;
 
+/* Migracion Usuarios */
 
-/*Roles Por Usuario*/
-BEGIN TRANSACTION
-INSERT INTO LPB.RolesxUsuarios(Roles_id, Usuarios_id) VALUES (1,1);
-COMMIT;
+INSERT INTO LPB.Usuarios (username, pass,TipoUsuario, nuevo)	
+SELECT DISTINCT @DocumentoCodigo_Cuit + REPLACE([Publ_Empresa_Cuit],'-','')
+	            ,'e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7'
+              	,@TipoDeUsuario_Empresa
+	            ,0	
+FROM [gd_esquema].[Maestra]
+WHERE [Publ_Empresa_Cuit] IS NOT NULL
+
+UNION ALL
+
+SELECT DISTINCT @DocumentoCodigo_Dni + CAST([Cli_Dni] AS VARCHAR(20))
+	            ,'e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7'	
+	            ,@TipoDeUsuario_Cliente
+	            ,0
+FROM [gd_esquema].[Maestra]
+WHERE [Cli_Dni] IS NOT NULL
+
+UNION
+
+SELECT DISTINCT @DocumentoCodigo_Dni + CAST([Publ_Cli_Dni] AS VARCHAR(20))
+	            ,'e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7'	
+	            ,@TipoDeUsuario_Cliente
+	            ,0
+FROM [gd_esquema].[Maestra]
+WHERE [Publ_Cli_Dni] IS NOT NULL
+
+
+/* Migracion de RolesxUsuario de los usuarios migrados en el paso anterior */
+INSERT INTO LPB.RolesxUsuarios (Usuarios_id, Roles_id)
+SELECT Id, CASE [TipoUsuario]
+		        WHEN @TipoDeUsuario_Administrador THEN @RolId_Administrador
+		        WHEN @TipoDeUsuario_Empresa THEN @RolId_Empresa
+		        WHEN @TipoDeUsuario_Cliente THEN @RolId_Cliente
+	       END
+FROM LPB.Usuarios
