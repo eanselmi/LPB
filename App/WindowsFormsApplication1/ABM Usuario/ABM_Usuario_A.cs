550059f6e79d3740d7ae7404825b9ef58afa53e1
bsdistefano <bsdistefano@gmail.com>
2016-05-22 20:33:10 -0300

    Agrego algunas validaciones al dar de alta Cliente

--- App/WindowsFormsApplication1/ABM Usuario/ABM_Usuario_A.cs
+++ App/WindowsFormsApplication1/ABM Usuario/ABM_Usuario_A.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 using Helper;
 using conectar;
 using System.Data.SqlClient;
@@ -92,6 +93,8 @@ namespace MercadoEnvio.ABM_Usuario
 
         private void buttonGuardar_Click(object sender, EventArgs e)
         {
+            //GUARDAR UN CLIENTE
+
             if (comboBoxRol.Text.Equals("Cliente"))
             {
                 // VALIDACION DE CAMPOS OBLIGATORIOS
@@ -101,7 +104,7 @@ namespace MercadoEnvio.ABM_Usuario
                     || textBoxTelefono.Text.Equals("") || textBoxCalleCl.Text.Equals("") || textBoxCodPostCl.Text.Equals("")
                     || textBoxNumeroDoc.Text.Equals("") || comboBoxTipoDoc.Text.Equals("") || textBoxNroCl.Text.Equals("")
                     || comboBoxLocalidades.Text.Equals("") || textBoxPisoCl.Text.Equals("") || textBoxFechaNac.Text.Equals("")
-                    ) 
+                    )
                 {
                     MessageBox.Show("Campos obligatorios vacios", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
@@ -112,7 +115,94 @@ namespace MercadoEnvio.ABM_Usuario
                       MessageBox.Show("Los campos Password y Confirmar Password no coinciden", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
+                //VALIDACION TIPO DE DATOS
+                //USERNAME
+                if (textBoxUser.Text.Length.CompareTo(45) == 1)
+                {
+                    MessageBox.Show("Username demasiado largo", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                //NOMBRE
+                if (!(textBoxNombre.Text.Equals("")))
+                {
+                    if (!(Regex.Match(textBoxNombre.Text, "^\\D+$").Success))
+                    {
+                        MessageBox.Show("Solo puede haber letras en el nombre", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                //APELLIDO
+                if (!(textBoxApellido.Text.Equals("")))
+                {
+                    if (!(Regex.Match(textBoxApellido.Text, "^\\D+$").Success))
+                    {
+                        MessageBox.Show("Solo puede haber letras en el apellido", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                //NUMERO DOC
+                if ((!(textBoxNumeroDoc.Text.Equals(""))) && comboBoxTipoDoc.Text.Equals("DNI")) 
+                {
+                    if (!(Regex.Match(textBoxNumeroDoc.Text, "^\\d+$").Success))
+                    {
+                        MessageBox.Show("Ingrese el DNI numérico y sin .", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    if (textBoxNumeroDoc.Text.Length.CompareTo(8) == 1)
+                    {
+                        MessageBox.Show("El DNI no puede ser tan largo", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                if ((!(textBoxNumeroDoc.Text.Equals(""))) && comboBoxTipoDoc.Text.Equals("Pasaporte"))
+                {
+                    if (!(Regex.Match(textBoxNumeroDoc.Text, "^[0-9A-Z]+$").Success))
+                    {
+                        MessageBox.Show("El número de pasaporte debe comprender letras y números", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    if (textBoxNumeroDoc.Text.Length.CompareTo(10) == 1)
+                    {
+                        MessageBox.Show("El número de pasaporte no puede ser tan largo", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                //MAIL
+
+                if(!(textBoxMail.Text.Equals("")))
+                {
+                    if(!(Regex.Match(textBoxMail.Text, "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,3})$").Success))
+                    {
+                        MessageBox.Show("El mail ingresado no es válido", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                //TELEFONO
+                if (!(textBoxTelefono.Text.Equals("")))
+                {
+                    if (!(Regex.Match(textBoxTelefono.Text, "^(011|15)\\d+$").Success))
+                    {
+                        MessageBox.Show("El número telefónico debe ser solo numérico y debe comenzar con 011 o 15", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    if (textBoxTelefono.Text.Length.CompareTo(11) == 1 || textBoxTelefono.Text.Length.CompareTo(10)==-1)
+                    {
+                        MessageBox.Show("El número de teléfono debe ser de 11 o 10 dígitos", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                //NUMERO DE CALLE
+
+                if (!(textBoxNroCl.Text.Equals("")))
+                {
+                    if (!(Regex.Match(textBoxNroCl.Text, "^\\d+$").Success))
+                    {
+                        MessageBox.Show("Ingrese el número de la dirección solo con números", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    if (textBoxNroCl.Text.Length.CompareTo(5) == 1)
+                    {
+                        MessageBox.Show("El número de la dirección debe ser como máximo de 5 dígitos", "Error al guardar el usuario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
                 return;
+
             }
 
             if (comboBoxRol.Text.Equals("Empresa"))
