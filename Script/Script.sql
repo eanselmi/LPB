5295b0903450b2b9d5de232b53325ae5ea14cf09
bsdistefano <bsdistefano@gmail.com>
2016-05-23 01:16:37 -0300

    Agrego SP para alta cliente y usuario

--- Script/Script.sql
+++ Script/Script.sql
@@ -339,6 +339,18 @@ BEGIN
 END;
 GO
 
+IF OBJECT_ID('LPB.SP_Alta_Usuario') IS NOT NULL
+BEGIN
+	DROP PROCEDURE LPB.SP_Alta_Usuario
+END;
+GO
+
+IF OBJECT_ID('LPB.SP_Alta_Cliente') IS NOT NULL
+BEGIN
+	DROP PROCEDURE LPB.SP_Alta_Cliente
+END;
+GO
+
 /*-------------- Definiciones de Stored Procedures ----------------*/
 
 CREATE PROCEDURE lpb.SP_Baja_Rol (@rol varchar(45), @id INT)
@@ -346,6 +358,27 @@ AS BEGIN
 UPDATE lpb.roles SET habilitado = 0 WHERE nombre = @rol
 DELETE lpb.funcionalidadesPorRol WHERE Rol_id = @id
 DELETE lpb.RolesPorUsuario WHERE Rol_id = @id
+END
+GO
+
+CREATE PROCEDURE lpb.SP_Alta_Usuario (@tipo varchar(25),@username varchar(45),@pass varchar(100))
+AS BEGIN
+INSERT INTO lpb.Usuarios
+(TipoUsuario,username,pass,habilitado,cantIntentosFallidos,nuevo)
+values
+(@tipo,@username,@pass,1,0,1)
+END
+GO
+
+CREATE PROCEDURE lpb.SP_Alta_Cliente (@numeroDoc numeric(18,0),@apellido nvarchar(255),@nombre nvarchar(255),@fechaNac datetime,
+@mail nvarchar(255),@telefono numeric(12,0), @calle nvarchar(255),@nroCalle numeric(18,0),@piso numeric(18,0),@dpto nvarchar(50),
+@codPostal nvarchar(50),@descrpLocalidad varchar(45), @user varchar(45))
+AS BEGIN
+INSERT INTO lpb.Clientes 
+(dni,apellido,nombre,fechaNacimiento,mail,telefono,domicilioCalle,nroCalle,piso,dpto,codPostal,Localidad_id,Usuario_id)
+select @numeroDoc,@apellido,@nombre,@fechaNac,@mail,@telefono,@calle,@nroCalle,@piso,@dpto,@codPostal,
+(select id from lpb.Localidades where descripcion=@descrpLocalidad),
+(select id from lpb.Usuarios where username=@user)
 END
 GO
 
